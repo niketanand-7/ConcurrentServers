2c4df9432bd3114cb32823ea42854f18a6afccf2
niketanand-7 <niketanand7@gmail.com>
2024-10-07 17:40:54 -0400

    Add socket binding to server implementation

--- sequential_server.cpp
+++ sequential_server.cpp
@@ -15,5 +15,21 @@ int main() {
     // 0: indicates the protocol (TCP)
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+    // binding socket to an ip address and port
+    struct sockaddr_in server_addr;
+    // &server_addr: pointer to the server_addr (which has the information about the server's ip address and port number)
+    // writing 0 to every block in the memory
+    // sizeof(server_addr): 
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET; // IPV4
+    server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to any available network interface
+    server_addr.sin_port = htons(PORT); // Convert port to network byte order
+
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        std::cerr << "Failed to bind socket" << std::endl;
+        close(server_socket);
+        return 1;
+    }
+
     
 }
