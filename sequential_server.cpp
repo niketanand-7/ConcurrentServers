5607bac7e76827f10ccc85557f0e33722bf26a95
niketanand-7 <niketanand7@gmail.com>
2024-10-07 17:27:55 -0400

    initializing socket

--- sequential_server.cpp
+++ sequential_server.cpp
@@ -7,3 +7,13 @@
 
 #define PORT 12345
 #define BUFFER_SIZE 1024
+
+int main() {
+    // create a socket using socket()
+    // AF_INET: indicates IPV4
+    // SOCK_STREAM: indicates TCP
+    // 0: indicates the protocol (TCP)
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    
+}
